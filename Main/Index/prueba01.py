62bd865f963bb30ec53c5058eb806992e905a690
juannsalto <115878705+juannsalto@users.noreply.github.com>
2022-11-07 18:17:57 -0300

    Subida
    Cambio de IPYNB A PY

---
+++ Main/Index/prueba01.py
@@ -0,0 +1,25 @@
+#############################
+# Primer commit main App-Prueba
+# *Subir a Github
+# * Index.py
+# ejecutable.exe
+# release: 26/10/2022
+# developer: Salto Juan Bautista
+##############################
+
+from ast import While
+from cgitb import reset
+from operator import concat
+from tkinter import N, Y
+
+#main-resources
+talles = "L", "M", "S", "X" "XL", "XXL" 
+
+modelo = "tiro ancho", "tiro bajo", "chupin", "acampanado", "roto", "corte inglés"
+
+#index
+talles = input('ingrese talle')
+print ('usted quiere, ' + talles)
+
+modelo = input('ingrese modelo')
+print ('usted quiere, ' + modelo)
